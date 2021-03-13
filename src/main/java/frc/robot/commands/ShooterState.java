04318a057e5d9f9ad6e96398f2d39225b468b77a
Tgaza <tagazawa55@gmail.com>
2021-03-13 10:31:15 -0600

    committing ShooterState

---
+++ src/main/java/frc/robot/commands/ShooterState.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.Shooter;
+
+public class ShooterState extends CommandBase {
+  /** Creates a new ShooterState. */
+  private final Shooter shooty;
+  private final Limelight limy;
+  private static boolean state = false;
+  public ShooterState(Shooter s, Limelight l) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    shooty = s;
+    limy = l;
+    addRequirements(shooty);
+    addRequirements(limy);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(state){
+      shooty.shooterRevUP(Constants.SHOOTER_IDLE);
+    }else{
+      //TODO: add the limelight call to replace LIMELIGHT CALL to get the necessary shooter speed
+      //shooty.shooterRevUP(LIMELIGHT CALL)
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+  public void stateToggle(){
+    state = !state;
+  }
+}
