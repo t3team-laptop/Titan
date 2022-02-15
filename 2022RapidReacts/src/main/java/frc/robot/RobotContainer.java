96da86bbdb8c8908146e429f846a45580c584094
DESKTOP-9JO4DE2\Daniel Marquez <merkytv22@gmail.com>
2022-02-15 09:46:15 -0600

    Fixed build issue with files. Added music player for falcon500 tone generators. Wii Song is default song. Can be changed.

--- 2022RapidReacts/src/main/java/frc/robot/RobotContainer.java
+++ 2022RapidReacts/src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.DriveForwardTimed;
 import frc.robot.commands.DriveWithJoysticks;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Jukebox;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -35,6 +36,10 @@ public class RobotContainer {
     driveForwardTimed.addRequirements(driveTrain);
 
     driverJoystick = new XboxController(Constants.JOYSTICK_NUMBER);
+    
+    //Start jukebox
+    Jukebox jukebox = new Jukebox();
+    jukebox.startJukebox();
 
     // Configure the button bindings
     //configureButtonBindings();
